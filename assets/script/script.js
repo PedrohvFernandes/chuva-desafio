ba971da5477ba1f576e04bcdc5f9dcbc30fae6cc
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2022-05-11 15:22:29 -0300

    Menu responsivo

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,35 @@
 function leiaMais() {
-    let pontos = document.getElementById('pontos');
-    let maisTexto = document.getElementById('mais');
-    let btnVerMais = document.getElementById('btnVerMais');
+  let pontos = document.getElementById("pontos");
+  let maisTexto = document.getElementById("mais");
+  let btnVerMais = document.getElementById("btnVerMais");
 
-    if(pontos.style.display === 'none'){
-      pontos.style.display = 'inline';
-      maisTexto.style.display = 'none';
-      btnVerMais.innerHTML = 'Ver mais';    
+  if (pontos.style.display === "none") {
+    pontos.style.display = "inline";
+    maisTexto.style.display = "none";
+    btnVerMais.innerHTML = "Ver mais";
+  } else {
+    pontos.style.display = "none";
+    maisTexto.style.display = "flex";
+    btnVerMais.innerHTML = "Ver menos";
+  }
+}
+
+// Função para abrir o modal de login
+function abrirModalUsuario() {
+  let modalUser = document.getElementById("modal-usuario-responsivo");
+  if (window.innerWidth <= 895) {
+    if (modalUser.style.display === "none") {
+      modalUser.style.display = "block";
     } else {
-      pontos.style.display = 'none';
-      maisTexto.style.display = 'flex';
-      btnVerMais.innerHTML = 'Ver menos';
+      modalUser.style.display = "none";
     }
+  } else {
+    modalUser.style.display = "none";
+  }
+}
+abrirModalUsuario();
+
+// Função para abrir o menu de navegação
+function abrirMenu() {
+  document.body.classList.toggle("menu-open")
 }
